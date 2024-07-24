957d95c25fd4d9c15326bc70115688a9a39ef2aa
John Cris <johncrismanabo9@gmail.com>
2024-07-24 14:59:22 +0800

    move queries to specific page

--- client/src/views/dashboard/Dashboard.js
+++ client/src/views/dashboard/Dashboard.js
@@ -4,68 +4,59 @@ import 'animate.css'
 import 'intro.js/introjs.css'
 import { jwtDecode } from 'jwt-decode'
 import PageTitle from 'src/components/PageTitle'
-import { api } from 'src/components/SystemConfiguration'
-import { useQueries, useQueryClient } from '@tanstack/react-query'
 import TopOfficeConsumption from './TopOfficeConsumption'
 import TotalConsumption from './TotalConsumption'
 import { CCol, CRow } from '@coreui/react'
 import TopDriverConsumption from './TopDriverConsumption'
 import TopEquipmentConsumption from './TopEquipmentConsumption'
 import OverallView from './OverallView'
+import TransactionDetails from './TransactionDetails'
+import RemaingBalancePercentage from './RemaingBalancePercentage'
+import TripTicketUserWork from './TripTicketUserWork'
+import OldTripTicketUserWork from './OldTripTicketUserWork'
 
 const Dashboard = ({ cardTitle }) => {
   const user = jwtDecode(localStorage.getItem('folomsToken'))
-  const [
-    totalConsumption,
-    topOfficeConsumption,
-    topDriverConsumption,
-    topEquipmentConsumption,
-    productConsumptionTrend,
-  ] = useQueries({
-    queries: [
-      {
-        queryKey: ['totalConsumption'],
-        queryFn: () => api.get('product/total_consumption').then((response) => response.data),
-      },
-      {
-        queryKey: ['topOfficeConsumption'],
-        queryFn: () => api.get('office/top_consumption').then((response) => response.data),
-      },
-      {
-        queryKey: ['topDriverConsumption'],
-        queryFn: () => api.get('driver/top_consumption').then((response) => response.data),
-      },
-      {
-        queryKey: ['topEquipmentConsumption'],
-        queryFn: () => api.get('equipment/top_consumption').then((response) => response.data),
-      },
 
-      {
-        queryKey: ['productConsumptionTrend'],
-        queryFn: () =>
-          api.get('trip_ticket/product_consumption_trend').then((response) => response.data),
-      },
-    ],
-  })
   return (
     <>
       <PageTitle pageTitle={cardTitle} />
 
-      <p>Welcome {user.first_name}</p>
+      <p>Welcome {user.first_name},</p>
 
-      <TotalConsumption totalConsumption={totalConsumption} />
-      <OverallView productConsumptionTrend={productConsumptionTrend} />
-      <CRow>
-        <CCol md={6}>
-          <TopOfficeConsumption topOfficeConsumption={topOfficeConsumption} />
-        </CCol>
-        <CCol md={6}>
-          <TopDriverConsumption topDriverConsumption={topDriverConsumption} />
+      <RemaingBalancePercentage />
+      {/* <RemaingBalance remainingBalance={remainingBalance} /> */}
+
+      {/* <TotalDelivery totalDelivery={totalDelivery} /> */}
+      <TotalConsumption />
+      <OverallView />
+
+      <CRow className="my-3">
+        <CCol md={12}>
+          <TransactionDetails />
         </CCol>
       </CRow>
-      <CRow className="mt-3">
+
+      <CRow className="my-1">
         <CCol md={6}>
-          <TopEquipmentConsumption topEquipmentConsumption={topEquipmentConsumption} />
+          <TripTicketUserWork />
+        </CCol>
+        <CCol md={6}>
+          <OldTripTicketUserWork />
+        </CCol>
+      </CRow>
+
+      <CRow className="mt-2">
+        <CCol md={6}>
+          <TopOfficeConsumption />
+        </CCol>
+        <CCol md={6}>
+          <TopDriverConsumption />
+        </CCol>
+      </CRow>
+      <CRow>
+        <CCol md={6}>
+          <TopEquipmentConsumption />
         </CCol>
       </CRow>
     </>
